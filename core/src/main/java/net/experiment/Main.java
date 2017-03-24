d68f347aff043548f633b2be8df7a61a900756c2
FAREAST\baliu <baliu@microsoft.com>
2017-03-24 15:18:07 +0800

    add Test

---
+++ core/src/main/java/net/experiment/Main.java
@@ -0,0 +1,11 @@
+package net.experiment;
+
+import net.experiment.test.Test000;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Test000.doTest(args);
+    }
+}
+
